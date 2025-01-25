c3c7760965809fec4ccc22afe81b818c08b1a611
Sandeep79-0 <2311cs010327@mallareddyuniversity.ac.in>
2025-01-25 09:49:25 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,18 @@
+public class demo2 {
+    public static void main(String args[]){
+        int a;
+        int b;
+        int a=30;
+        int b=70;
+
+        if(a>b){
+            System.out.println(x:"a is greatest");
+        }
+        else{
+            System.out.println(x:"{b is greatest")
+   
+        }
+    }
+    }
+    
+
